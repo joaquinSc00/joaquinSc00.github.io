0c5149939b5469fd53c29bfe72ec9ebfd593396e
joaquinSc00 <104645222+joaquinSc00@users.noreply.github.com>
2026-03-17 17:28:36 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -1,12 +1,13 @@
 const horario = [
-  { dia: 1, hora: "13:15", materia: "Inglés II" },
-  { dia: 2, hora: "18:15", materia: "Dispositivos Electrónicos (Sigampa)" },
-  { dia: 2, hora: "19:55", materia: "Teoría de los Circuitos I (Peluca)" },
-  { dia: 3, hora: "15:40", materia: "Análisis de Señales y Sistemas" },
-  { dia: 3, hora: "18:15", materia: "Teoría de los Circuitos I (Peluca)" },
-  { dia: 3, hora: "20:40", materia: "Electrónica Aplicada I (Gilberto)" },
-  { dia: 4, hora: "15:40", materia: "Análisis de Señales y Sistemas" },
-  { dia: 4, hora: "18:15", materia: "Electrónica Aplicada I (Rivas)" },
-  { dia: 5, hora: "18:15", materia: "Seguridad e Higiene" },
-  { dia: 5, hora: "20:40", materia: "Dispositivos Electrónicos (Guanuco)" }
+  { dia: 1, hora: "17:20", materia: "Medidas Electrónicas I" },
+  { dia: 1, hora: "19:00", materia: "Sistemas de Comunicaciones" },
+  { dia: 1, hora: "20:40", materia: "Electrónica Aplicada II" },
+  { dia: 2, hora: "18:15", materia: "Máquinas e Instalaciones Eléctricas" },
+  { dia: 2, hora: "21:35", materia: "Técnicas Digitales II" },
+  { dia: 3, hora: "19:00", materia: "Electrónica Aplicada II" },
+  { dia: 3, hora: "20:40", materia: "Teoría de los Circuitos II" },
+  { dia: 4, hora: "18:15", materia: "Técnicas Digitales II" },
+  { dia: 4, hora: "20:40", materia: "Medidas Electrónicas I" },
+  { dia: 5, hora: "18:15", materia: "Teoría de los Circuitos II" },
+  { dia: 5, hora: "19:55", materia: "Sistemas de Comunicaciones" }
 ];
