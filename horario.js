8a73b6ebc8fac74169e788ceda390ebf29eeb29f
joaquinSc00 <104645222+joaquinSc00@users.noreply.github.com>
2026-03-17 17:59:13 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -1,13 +1,90 @@
 const horario = [
-  { dia: 1, hora: "17:20", materia: "Medidas Electrónicas I" },
-  { dia: 1, hora: "19:00", materia: "Sistemas de Comunicaciones" },
-  { dia: 1, hora: "20:40", materia: "Electrónica Aplicada II" },
-  { dia: 2, hora: "18:15", materia: "Máquinas e Instalaciones Eléctricas" },
-  { dia: 2, hora: "21:35", materia: "Técnicas Digitales II" },
-  { dia: 3, hora: "19:00", materia: "Electrónica Aplicada II" },
-  { dia: 3, hora: "20:40", materia: "Teoría de los Circuitos II" },
-  { dia: 4, hora: "18:15", materia: "Técnicas Digitales II" },
-  { dia: 4, hora: "20:40", materia: "Medidas Electrónicas I" },
-  { dia: 5, hora: "18:15", materia: "Teoría de los Circuitos II" },
-  { dia: 5, hora: "19:55", materia: "Sistemas de Comunicaciones" }
+  {
+    dia: 1,
+    inicio: "17:20",
+    fin: "18:50",
+    materia: "Medidas Electronicas I",
+    detalle: "4R1",
+    color: "medidas"
+  },
+  {
+    dia: 1,
+    inicio: "19:00",
+    fin: "20:30",
+    materia: "Sistemas de Comunicaciones",
+    detalle: "",
+    color: "comunicaciones"
+  },
+  {
+    dia: 1,
+    inicio: "20:40",
+    fin: "22:55",
+    materia: "Electronica Aplicada II",
+    detalle: "4R1",
+    color: "aplicada"
+  },
+  {
+    dia: 2,
+    inicio: "18:15",
+    fin: "21:15",
+    materia: "Maquinas e Instalaciones Electricas",
+    detalle: "4R1",
+    color: "maquinas"
+  },
+  {
+    dia: 2,
+    inicio: "21:35",
+    fin: "23:05",
+    materia: "Tecnicas Digitales II",
+    detalle: "4R1",
+    color: "digitales"
+  },
+  {
+    dia: 3,
+    inicio: "19:00",
+    fin: "20:30",
+    materia: "Electronica Aplicada II",
+    detalle: "4R1",
+    color: "aplicada"
+  },
+  {
+    dia: 3,
+    inicio: "20:40",
+    fin: "22:55",
+    materia: "Teoria de los Circuitos II",
+    detalle: "4R1",
+    color: "circuitos"
+  },
+  {
+    dia: 4,
+    inicio: "18:15",
+    fin: "20:30",
+    materia: "Tecnicas Digitales II",
+    detalle: "4R1",
+    color: "digitales"
+  },
+  {
+    dia: 4,
+    inicio: "20:40",
+    fin: "22:55",
+    materia: "Medidas Electronicas I",
+    detalle: "4R1",
+    color: "medidas"
+  },
+  {
+    dia: 5,
+    inicio: "18:15",
+    fin: "19:45",
+    materia: "Teoria de los Circuitos II",
+    detalle: "",
+    color: "circuitos"
+  },
+  {
+    dia: 5,
+    inicio: "19:55",
+    fin: "21:25",
+    materia: "Sistemas de Comunicaciones",
+    detalle: "",
+    color: "comunicaciones"
+  }
 ];
